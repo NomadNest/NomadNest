3d4734ac5a26fd36ec9ce24d1ebc7e1e77bf7873
Chiara888 <carolin@family-klose.de>
2023-06-09 18:41:56 +0200

    testing the cowork with Emilio

--- routes/nest.routes.js
+++ routes/nest.routes.js
@@ -4,19 +4,13 @@ const router = express.Router();
 const Nest = require('../models/Nest.model');
 
 
-
 // READ: display all nests
 
 router.get("/", (req, res, next)=>{
-    res.send("Hello Nests")
+    res.send("Hi Emilio, lets see if it is working :)")
     console.log("Hello")
-
 })
 
-router.get("/hello", (req, res, next)=>{
-    res.send("Hello Hellooooo")
-    console.log("Hello")
 
-})
 
 module.exports = router;
