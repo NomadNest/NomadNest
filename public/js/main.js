71239503344f759a23f90cc4f11293516390af2d
Emilio Moreno <emilio@Emilios-MacBook-Air.local>
2023-06-14 15:04:31 +0200

    commented google API

--- public/js/main.js
+++ public/js/main.js
@@ -1,50 +1,50 @@
-window.addEventListener('load', () => {
-    const ironhackBCN = {
-      lat: 41.38623,
-      lng: 2.17498
-    };
+// window.addEventListener('load', () => {
+//     const ironhackBCN = {
+//       lat: 41.38623,
+//       lng: 2.17498
+//     };
   
-    const map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 13,
-      center: ironhackBCN
-    });
-  });
+//     const map = new google.maps.Map(document.getElementById('map'), {
+//       zoom: 13,
+//       center: ironhackBCN
+//     });
+//   });
   
 
 
-// public/javascripts/main.js
+// // public/javascripts/main.js
 
-function getRestaurants() {
-  axios
-    .get('/restaurants/api')
-    .then(response => {
-      placeRestaurants(response.data.restaurants);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-}
+// function getRestaurants() {
+//   axios
+//     .get('/restaurants/api')
+//     .then(response => {
+//       placeRestaurants(response.data.restaurants);
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     });
+// }
 
 
-// render the markers from the main.js file:
+// // render the markers from the main.js file:
 
-// public/javascripts/main.js
+// // public/javascripts/main.js
 
-function placeRestaurants(restaurants) {
-  for (let restaurant of restaurants) {
-    const center = {
-      lat: restaurant.location.coordinates[1],
-      lng: restaurant.location.coordinates[0]
-    };
-    const pin = new google.maps.Marker({
-      position: center,
-      map: map,
-      title: restaurant.name
-    });
-    markers.push(pin);
-  }
-}
+// function placeRestaurants(restaurants) {
+//   for (let restaurant of restaurants) {
+//     const center = {
+//       lat: restaurant.location.coordinates[1],
+//       lng: restaurant.location.coordinates[0]
+//     };
+//     const pin = new google.maps.Marker({
+//       position: center,
+//       map: map,
+//       title: restaurant.name
+//     });
+//     markers.push(pin);
+//   }
+// }
 
-getRestaurants();
+// getRestaurants();
 
 
